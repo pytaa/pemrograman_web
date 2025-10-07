7e1dddde9f8f6dd621a2fb0b57fab4f3f6faa10b
pytaa <pytanur@gmail.com>
2025-10-07 15:19:47 +0800

    Login page bagus

--- login.js
+++ login.js
@@ -1,16 +1,29 @@
+localStorage.setItem("isLoggedIn", "true");
+
 const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  // Kredensial sederhana
-  if (username === "admin" && password === "12345") {
+  const validUsername = "admin";
+  const validPassword = "12345";
+
+  // Cek kondisi satu per satu
+  if (username === validUsername && password === validPassword) {
     localStorage.setItem("isLoggedIn", "true");
-    window.location.href = "index.html";
-  } else {
-    errorMsg.textContent = "Username atau password salah!";
+    window.location.href = "latihan-html.html";
+  } 
+  else if (username !== validUsername && password === validPassword) {
+    errorMsg.textContent = "Username salah!";
+  } 
+  else if (username === validUsername && password !== validPassword) {
+    errorMsg.textContent = "Password salah!";
+  } 
+  else {
+    errorMsg.textContent = "Username dan password salah!";
   }
 });
